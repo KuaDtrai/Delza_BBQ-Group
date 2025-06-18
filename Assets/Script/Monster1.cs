8bc137db7ce6bd18785d4a57c96dc537f575f5bb
lan-dang <thienlan100@gmail.com>
2025-06-18 21:35:22 +0700

    Monster Animation

--- Assets/Script/Monster1.cs
+++ Assets/Script/Monster1.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class Monster1 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject player;
+    public float speed;
+    public float distanceBetween;
+    public int damage;
+
+    private float distance;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+
+        if (player.transform.position.x < 0) // Moving left
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        else if (player.transform.position.x > 0) // Moving right
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+
+        if (distance < distanceBetween) 
+        { 
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+
+        }
     }
 }
